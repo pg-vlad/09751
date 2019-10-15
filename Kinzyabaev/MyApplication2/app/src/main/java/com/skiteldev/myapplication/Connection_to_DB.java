9ef58a10b4a97c6296de48ce89d95c5d26a745b4
Skiteri <skitelkin1999@gmail.com>
2019-10-15 21:39:20 +0300

    complete Connection_to_DB

--- Kinzyabaev/MyApplication2/app/src/main/java/com/skiteldev/myapplication/Connection_to_DB.java
+++ Kinzyabaev/MyApplication2/app/src/main/java/com/skiteldev/myapplication/Connection_to_DB.java
@@ -1,9 +1,55 @@
 package com.skiteldev.myapplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Connection_to_DB {
     private static final String URL = "your url";
     private static final String USER = "your username";
     private static final String PASSWORD = "your pass";
 
-    
+    private static Connection connection;
+    private static PreparedStatement preparedStatement;
+    private static ResultSet set;
+
+    public static void setPreparedStatement(PreparedStatement preparedStatement) {
+        Connection_to_DB.preparedStatement = preparedStatement;
+    }
+
+    public static void setResultSet(ResultSet set) {
+        Connection_to_DB.set = set;
+    }
+
+    public Connection connect() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            connection = null;
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void close_DB() {
+        try {
+            connection.close();
+            connection = null;
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
