b9e1d00dca737fb28319bf3160e1a7f0820e4925
Skiteri <skitelkin1999@gmail.com>
2019-10-15 21:24:51 +0300

    rename class

--- Kinzyabaev/MyApplication2/app/src/main/java/com/skiteldev/myapplication/Connection_to_DB.java
+++ Kinzyabaev/MyApplication2/app/src/main/java/com/skiteldev/myapplication/Connection_to_DB.java
@@ -1,6 +1,6 @@
 package com.skiteldev.myapplication;
 
-public class Connection {
+public class Connection_to_DB {
     private static final String URL = "your url";
     private static final String USER = "your username";
     private static final String PASSWORD = "your pass";
