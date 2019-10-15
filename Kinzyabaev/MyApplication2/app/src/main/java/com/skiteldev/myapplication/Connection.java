16c5150325d0abf01a2eca3c52eef7a3b1bc8f5c
Skiteri <skitelkin1999@gmail.com>
2019-10-15 21:18:26 +0300

    add connection class

---
+++ Kinzyabaev/MyApplication2/app/src/main/java/com/skiteldev/myapplication/Connection.java
@@ -0,0 +1,9 @@
+package com.skiteldev.myapplication;
+
+public class Connection {
+    private static final String URL = "your url";
+    private static final String USER = "your username";
+    private static final String PASSWORD = "your pass";
+
+    
+}
